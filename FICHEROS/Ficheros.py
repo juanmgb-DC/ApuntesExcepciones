304efe2d19cbff70ce16931024916814b45d9eb7
Juan <j.martinezgonzalez-besada@danielcastelao.org>
2026-01-15 14:35:21 +0100

    Commit 4 - Ficheros

---
+++ FICHEROS/Ficheros.py
@@ -0,0 +1,22 @@
+import datetime
+
+ficheiro = open('/home/dam/salida.txt')
+
+for linha in ficheiro:
+    print(linha)
+
+ficheiro.close()
+
+data_hora= (datetime.datetime.now())
+data, hora =str(data_hora).split()
+
+ficheiro2 = open('saudo.txt','w')
+
+ficheiro2.write("Hola, soy Juan\n")
+ficheiro2.write(data)
+ficheiro2.write('\n')
+ficheiro2.write(hora)
+ficheiro2.write('\n')
+ficheiro2.write("Acabo y cierro fichero")
+
+ficheiro2.close()
